9726523ae035e9ec02ab16cefc76ee57ecdca675
Bogomil Rusev <trippyboki@gmail.com>
2023-05-08 12:42:14 +0300

    Added functionality

--- Prototype Task/BankingApp.js
+++ Prototype Task/BankingApp.js
@@ -1,6 +1,6 @@
-function BankAccount() {
-        this.account = document.querySelector('#account-holder');
-        this.balance = document.querySelector('#balance');
+function BankAccount(accHolder, balance) {
+        this.account = accHolder;
+        this.balance = balance;
     };
 
     BankAccount.prototype.deposit = function(amount) {
@@ -16,3 +16,40 @@ function BankAccount() {
             alert('Withdraw maximum exceeded')
     }
 }
+
+let accounts = [];
+
+let submit = document.querySelector('input[type="submit"]');
+submit.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    let accHolder = document.querySelector('#account-name').value;
+    let balance = parseInt(document.querySelector('#balance').value);
+
+    let newAcc = new BankAccount(accHolder, balance);
+    console.log(newAcc);
+    accounts.push(newAcc);
+
+    let depositFunc = document.querySelector('#deposit-field');
+    let withdrawFunc = document.querySelector('#withdraw-field');
+
+    depositFunc.removeAttribute('hidden');
+    withdrawFunc.removeAttribute('hidden');
+});
+
+    let depositBtn = document.querySelector('#deposit-btn');
+    let withdrawBtn = document.querySelector('#withdraw-btn');
+
+    depositBtn.addEventListener('click', (ev) => {
+        ev.preventDefault();
+
+        let depositAmount = parseInt(document.querySelector('#deposit').value);
+        accounts[accounts.length - 1].deposit(depositAmount);
+    });
+
+    withdrawBtn.addEventListener('click', (ev) => {
+        ev.preventDefault();
+
+        let withdrawAmount = parseInt(document.querySelector('#withdraw').value);
+        accounts[accounts.length - 1].withdraw(withdrawAmount);
+    });
