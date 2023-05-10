4761ec99bd00b4a3a039e1e3e676852fd2b51ee1
Bogomil Rusev <trippyboki@gmail.com>
2023-05-10 14:01:21 +0300

    Quiz 1 files

---
+++ Quiz.1.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+
+    const startHour = Number(arr[0]);
+    const endHour = Number(arr[1]);
+    const hourlyRate = Number(arr[2]);
+    const overtimePay = Number(arr[3]);
+
+    const normalHours = Math.min(endHour, 17) - Math.max(startHour, 9);
+    const overtimehours = Math.max(endHour, 17) - Math.max(startHour, 17);
+
+    const regularWage = normalHours * hourlyRate;
+    const overtimeWage = overtimehours * hourlyRate * overtimePay;
+
+    console.log((regularWage + overtimeWage).toFixed(2));
+}
+
+solve([13.25, 15, 30, 1.5]);
